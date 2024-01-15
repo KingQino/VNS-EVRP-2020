9fdf246749a4b4cca41e6d176a726293dbd92d60
Yinghao Qin <yh.qin@foxmail.com>
2024-01-15 10:52:25 +0000

    final version

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
 using namespace std;
 
 
-/*initialiazes a run for your heuristic*/
+/*initializes a run for your heuristic*/
 void start_run(int r) {
 
     srand(r); //random seed
@@ -46,77 +46,77 @@ bool termination_condition(void) {
 /****************************************************************/
 /*                Main Function                                 */
 /****************************************************************/
-//int main(int argc, char *argv[]) {
-//    int run;
-//    if (argc < 3) {
-//        std::cerr << "Usage: " << argv[0] << " <problem_instance_filename> <seed>\n";
-//        return 1; // Exit with an error code
-//    }
-//
-//    std::string instanceName(argv[1]);
-//    run = std::stoi(argv[2]);
-//
-//    problem_instance = argv[1];       //pass the .evrp filename as an argument
-//    read_problem(problem_instance);   //Read EVRP from file from EVRP.h
-//
-//
-//    string instancePrefix = instanceName.substr(0, instanceName.find_last_of('.'));
-//    string directoryPath = StatsInterface::statsPath+ "/" + instancePrefix  + "/" + to_string(run);
-//
-//    start_run(run);
-//    initialize_heuristic(); //heuristic.h
-//    run_heuristic();  //heuristic.h
-//    end_run(run);  //store the best solution quality for each run
-//
-//
-//    free_stats();
-//    free_heuristic();
-//    free_EVRP();
-//
-//    return 0;
-//}
-
-
 int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " <problem_instance_filename>\n";
+    int run;
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " <problem_instance_filename> <seed>\n";
         return 1; // Exit with an error code
     }
 
     std::string instanceName(argv[1]);
+    run = std::stoi(argv[2]);
 
-    vector<double> data;
+    problem_instance = argv[1];       //pass the .evrp filename as an argument
+    read_problem(problem_instance);   //Read EVRP from file from EVRP.h
+
+
     string instancePrefix = instanceName.substr(0, instanceName.find_last_of('.'));
-    string directoryPrefix = StatsInterface::statsPath+ "/" + instancePrefix  + "/";
-    for (int run = 1; run <= 20; ++run) {
-        string directoryPath = directoryPrefix + to_string(run);
+    string directoryPath = StatsInterface::statsPath+ "/" + instancePrefix  + "/" + to_string(run);
 
-        // Construct the filename based on the run number
-        string filename = "solution." + to_string(run) + "." + instancePrefix + ".txt";
-        string filepath = directoryPath + "/" + filename;
+    start_run(run);
+    initialize_heuristic(); //heuristic.h
+    run_heuristic();  //heuristic.h
+    end_run(run);  //store the best solution quality for each run
 
-        // Open the file
-        ifstream infile(filepath);
 
-        if (infile) {
-            double value;
-            // Read the first line from the file and convert it to double
-            if (infile >> value) {
-                // Push the value to the data vector
-                data.push_back(value);
-                cout << "Run " << run << ": Value read from file " << filename << ": " << value << endl;
-            } else {
-                cerr << "Error reading value from file: " << filename << endl;
-            }
-            infile.close();
-        } else {
-            cerr << "Error opening file: " << filename << endl;
-        }
-    }
-
-    string statsFileName = directoryPrefix + "stats." + instancePrefix + ".txt";
-    StatsInterface::stats_for_multiple_trials(statsFileName, data);
-
+    free_stats();
+    free_heuristic();
+    free_EVRP();
 
     return 0;
 }
+
+
+//int main(int argc, char *argv[]) {
+//    if (argc < 2) {
+//        std::cerr << "Usage: " << argv[0] << " <problem_instance_filename>\n";
+//        return 1; // Exit with an error code
+//    }
+//
+//    std::string instanceName(argv[1]);
+//
+//    vector<double> data;
+//    string instancePrefix = instanceName.substr(0, instanceName.find_last_of('.'));
+//    string directoryPrefix = StatsInterface::statsPath+ "/" + instancePrefix  + "/";
+//    for (int run = 1; run <= 20; ++run) {
+//        string directoryPath = directoryPrefix + to_string(run);
+//
+//        // Construct the filename based on the run number
+//        string filename = "solution." + to_string(run) + "." + instancePrefix + ".txt";
+//        string filepath = directoryPath + "/" + filename;
+//
+//        // Open the file
+//        ifstream infile(filepath);
+//
+//        if (infile) {
+//            double value;
+//            // Read the first line from the file and convert it to double
+//            if (infile >> value) {
+//                // Push the value to the data vector
+//                data.push_back(value);
+//                cout << "Run " << run << ": Value read from file " << filename << ": " << value << endl;
+//            } else {
+//                cerr << "Error reading value from file: " << filename << endl;
+//            }
+//            infile.close();
+//        } else {
+//            cerr << "Error opening file: " << filename << endl;
+//        }
+//    }
+//
+//    string statsFileName = directoryPrefix + "stats." + instancePrefix + ".txt";
+//    StatsInterface::stats_for_multiple_trials(statsFileName, data);
+//
+//
+//    return 0;
+//}
